08484401b4f3fa88baf4acedd812c24d7e7d46b7
harleenkaur05 <65458410+harleenkaur05@users.noreply.github.com>
2020-05-31 12:10:09 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 (function (window) {
-	var helloSpeak = {};
+	var helloSpeak = new Object();
 	var greeting = "Hello ";
 	helloSpeak.sayhello= function(name) {
 		console.log(greeting + " " + name);
